7e9546c21c6ee9dfbfa6efdb1ac74dce964e7c13
Nico Martinucci <domenico.martinucci@gmail.com>
2023-01-16 16:55:39 -0800

    finish code review

--- src/App.js
+++ src/App.js
@@ -4,16 +4,10 @@ import Pokegame from './Pokegame';
 
 function App() {
   return (
-    <Pokegame pokemans={[
-      {id: 4,   name: 'Charmander', type: 'fire',     base_experience: 62},
-      {id: 7,   name: 'Squirtle',   type: 'water',    base_experience: 63},
-      {id: 11,  name: 'Metapod',    type: 'bug',      base_experience: 72},
-      {id: 12,  name: 'Butterfree', type: 'bug, flying',   base_experience: 178},
-      {id: 25,  name: 'Pikachu',    type: 'electric', base_experience: 112},
-      {id: 39,  name: 'Jigglypuff', type: 'normal',   base_experience: 95},
-      {id: 94,  name: 'Gengar',     type: 'ghost, poison',   base_experience: 225},
-      {id: 133, name: 'Eevee',      type: 'normal',   base_experience: 65}
-    ]}/>
+    <div>
+      <Pokegame />
+      <Pokegame />
+    </div>
   );
 }
 
--- src/Pokegame.js
+++ src/Pokegame.js
@@ -2,12 +2,23 @@ import Pokedex from "./Pokedex";
 import "./Pokegame.css"
 import { assignRandomHands, calcWinningHand } from "./helpers";
 
+const DEFAULT_POKEMANS = [
+    {id: 4,   name: 'Charmander', type: 'fire',     base_experience: 62},
+    {id: 7,   name: 'Squirtle',   type: 'water',    base_experience: 63},
+    {id: 11,  name: 'Metapod',    type: 'bug',      base_experience: 72},
+    {id: 12,  name: 'Butterfree', type: 'bug, flying',   base_experience: 178},
+    {id: 25,  name: 'Pikachu',    type: 'electric', base_experience: 112},
+    {id: 39,  name: 'Jigglypuff', type: 'normal',   base_experience: 95},
+    {id: 94,  name: 'Gengar',     type: 'ghost, poison',   base_experience: 225},
+    {id: 133, name: 'Eevee',      type: 'normal',   base_experience: 65}
+  ]
+
 /**
  * Pokegame: component containing two Pokedexes of randomly shuffled pokemans
  * @param {*} param0
  * @returns
  */
-function Pokegame({ pokemans }) {
+function Pokegame({ pokemans=DEFAULT_POKEMANS }) {
     const [handOne, handTwo] = assignRandomHands(pokemans);
 
     const gameResults = calcWinningHand(handOne, handTwo)
